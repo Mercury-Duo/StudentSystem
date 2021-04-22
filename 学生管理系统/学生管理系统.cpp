094a413b2fc39b611e01cf1be73f6be90698e368
铎 <@DESKTOP-733MSEH>
2021-04-22 17:44:00 +0800

    null

--- 学生管理系统/学生管理系统.cpp
+++ 学生管理系统/学生管理系统.cpp
@@ -21,19 +21,27 @@ void CreatNodeList(struct Node* HeadNode, int data) {
 	NewNode->Next = HeadNode->Next;
 	HeadNode->Next = NewNode;
  }
-void SystemMenu(){}
+bvoid SystemMenu(){}
 void DeleteNode(struct Node* StudentNode,int data){
-	struct Node* x = StudentNode;
-	struct Node* y = StudentNode->Next;
-	while(StudentNode->data==data){
-		
-	}
-
+	if (StudentNode == NULL)return;
+	struct Node* FrontNode = StudentNode;
+	struct Node* PosNode = StudentNode->Next;
+	while (PosNode->data != data) {
+		PosNode->Next = PosNode;
+		PosNode = PosNode->Next;
+		if (PosNode == NULL) {
+			printf("无相关内容\n");
+			return;
+		}
+}
+	PosNode->Next = PosNode->Next;
+	free(PosNode);
 }
 void PrintNode(struct Node* StudentNode){
 	struct Node* pow = StudentNode->Next;
 	while(pow){
 		printf("%d",pow->data);
+		printf("\n");
 		pow = pow->Next;
 	}
 }
@@ -42,6 +50,8 @@ int main(void) {
 	CreatNodeList(StudentNode, 1);
 	CreatNodeList(StudentNode, 2);
 	CreatNodeList(StudentNode, 3);
+	PrintNode(StudentNode);
+	DeleteNode(StudentNode,2);
 	PrintNode(StudentNode);
 
 }
