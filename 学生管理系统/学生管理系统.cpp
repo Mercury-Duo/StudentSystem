89d9a9caa91cc1c8080453b0c89814b7fa2bd96d
铎 <@DESKTOP-733MSEH>
2021-04-22 19:13:44 +0800

    null

--- 学生管理系统/学生管理系统.cpp
+++ 学生管理系统/学生管理系统.cpp
@@ -21,21 +21,35 @@ void CreatNodeList(struct Node* HeadNode, int data) {
 	NewNode->Next = HeadNode->Next;
 	HeadNode->Next = NewNode;
  }
-void SystemMenu(){}
-void DeleteNode(struct Node* StudentNode,int data){
-	if (StudentNode == NULL)return;
+//void SystemMenu(){}
+/*void DeleteNode(struct Node* StudentNode,int data){
 	struct Node* FrontNode = StudentNode;
 	struct Node* PosNode = StudentNode->Next;
 	while (PosNode->data != data) {
-		PosNode->Next = PosNode;
+		FrontNode->Next = PosNode;
 		PosNode = PosNode->Next;
-		if (PosNode == NULL) {
-			printf("无相关内容\n");
-			return;
 		}
-}
-	PosNode->Next = PosNode->Next;
+
+	FrontNode->Next = PosNode->Next;
 	free(PosNode);
+}*/
+void deletenode(struct Node* headnode, int x) {
+	struct Node* posnode = headnode->Next;
+	struct Node* posnodefront = headnode;
+	if (posnode->data == NULL) {
+		printf("链表为空，无法删除");
+
+	}
+	else
+	{
+		while (posnode->data != x) {
+			posnodefront = posnode;
+			posnode = posnodefront->Next;
+			if (posnode == NULL) { printf("未找到指定位置，无法删除"); return; }
+		}
+		posnodefront->Next = posnode->Next;
+		free(posnode);
+	}
 }
 void PrintNode(struct Node* StudentNode){
 	struct Node* pow = StudentNode->Next;
@@ -51,7 +65,8 @@ int main(void) {
 	CreatNodeList(StudentNode, 2);
 	CreatNodeList(StudentNode, 3);
 	PrintNode(StudentNode);
-	DeleteNode(StudentNode,2);
+	//DeleteNode(StudentNode,1);
+	deletenode(StudentNode,1);
 	PrintNode(StudentNode);
 
 }
