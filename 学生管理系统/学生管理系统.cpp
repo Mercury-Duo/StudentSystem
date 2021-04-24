e3c32a436fa4ea9305aa3bebcdadaee70a1b7276
铎 <@DESKTOP-733MSEH>
2021-04-24 10:51:54 +0800

    新增修改信息函数

--- 学生管理系统/学生管理系统.cpp
+++ 学生管理系统/学生管理系统.cpp
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<String.h>
+//初始化所有函数
 void SystemMenu();
 void KeyRecive();
 void InsertNode();
@@ -31,7 +32,7 @@ void SystemMenu() {
 	printf("\t\t\t1.浏览信息\n");
 	printf("\t\t\t2.删除信息\n");
 	printf("\t\t\t3.修改信息\n");
-	printf("\t\t\t3.查找信息\n");
+	printf("\t\t\t4.查找信息\n");
 	printf("\t\t\t5.退出系统\n");
 	printf("-----------------------------------------------------------------------------------\n");
 
@@ -43,7 +44,7 @@ void KeyRecive() {
 	{
 	case 0:
 		InsertNode();
-		printf("插入操作执行成功！-------------");
+		printf("插入操作执行完成！-------------");
 		break;
 	case 1:
 		printf("\t【浏览信息】\n");
@@ -57,8 +58,19 @@ void KeyRecive() {
 		break;
 	case 3:
 		printf("\t【更改信息】\n");
-		
-		printf("修改操作执行完成---------------");
+		printf("请输入更改的姓名：");
+		scanf("%s", &s.name);
+		if (SearchNode(StudentNode, s.name) == NULL) {
+			printf("未找到相关信息！\n");
+		}
+		else
+		{
+			struct Node* s1 = SearchNode(StudentNode, s.name);
+			printf("请输入新的学生信息");
+			printf("请输入同学姓名，年龄，电话，住址");
+			scanf("%s%d%s%s", &s1->data.name, &s1->data.age, &s1->data.tel, &s1->data.addr);
+			printf("修改操作执行完成---------------");
+		}
 		break;
 	case 5:
 		printf("\t【退出系统】\n");
@@ -74,14 +86,15 @@ void KeyRecive() {
 		else
 		{
 			PrintList(SearchNode(StudentNode, s.name));
+			printf("查找操作执行完成---------------");
 		}
-		printf("查找操作执行完成---------------");
+		
 		break;
 	default:printf("输入数据有误，重新输入");KeyRecive();
 		break;
 	}
 }
-void InsertNode() {
+void InsertNode() {//插入函数
     int x;
 	struct Student S;
 	printf("请输入学生个数");
@@ -92,30 +105,30 @@ void InsertNode() {
 		CreatNodeList(StudentNode,S);
 	}
 }
-struct Node* CreatNode() 
+struct Node* CreatNode() //建立头节点
 {
 	struct Node* HeadNode = (struct Node*)malloc(sizeof(struct Node));
 	HeadNode->Next = NULL;
 	return HeadNode;
 }
-struct Node* CreatNewNode(struct Student data) {
+struct Node* CreatNewNode(struct Student data) {//建立新结点函数
 	struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node));
 	NewNode->data = data;
 	NewNode->Next = NULL;
 	return NewNode;
 }
-void CreatNodeList(struct Node* HeadNode, struct Student data) {
+void CreatNodeList(struct Node* HeadNode, struct Student data) {//建立新节点并插入数据
 	struct Node* NewNode = CreatNewNode(data);
 	NewNode->Next = HeadNode->Next;
 	HeadNode->Next = NewNode;
  }
-void deletestudent() {
+void deletestudent() {//删除节点|
 	char name[10];
 	printf("请输入需要删除的学生名字");
 	scanf("%s",&name);
 	deletenode(StudentNode,name);
 }
-void deletenode(struct Node* headnode, char *name) {
+void deletenode(struct Node* headnode, char *name) {//删除节点
 	struct Node* posnode = headnode->Next;
 	struct Node* posnodefront = headnode;
 	if (posnode == NULL) {
@@ -133,7 +146,7 @@ void deletenode(struct Node* headnode, char *name) {
 		free(posnode);
 	}
 }
-struct Node* SearchNode(struct Node* StudentNode, char* name) {
+struct Node* SearchNode(struct Node* StudentNode, char* name) {//查找节点
 	struct Node* pMove = StudentNode->Next;
 	if (pMove == NULL) { 
 		printf("未找到该学生");
@@ -148,11 +161,11 @@ struct Node* SearchNode(struct Node* StudentNode, char* name) {
 		return pMove;
 	}
 }
-void PrintList(struct Node* StudentNode) {
+void PrintList(struct Node* StudentNode) {//打印单个节点
 	printf("姓名\t年龄\t电话\t住址\n");
 	printf("%s\t%d\t%s\t%s\n",StudentNode->data.name,StudentNode->data.age,StudentNode->data.tel,StudentNode->data.addr);
 }
-void PrintNode(struct Node* StudentNode){
+void PrintNode(struct Node* StudentNode){//浏览信息
 	struct Node* pow = StudentNode->Next;
 	printf("姓名\t年龄\t电话\t住址\n");
 	while(pow){
